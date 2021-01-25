350416f36a457fcc699c6f370d459029a94f514e
Aylin Ünal <ayl.uenal@gmail.com>
2021-01-25 11:38:19 +0100

    add basic html structure, basic css and card deck in JS

---
+++ cardDeck.js
@@ -0,0 +1,54 @@
+const cards = [
+
+    {
+        image: "snake.jpg",
+        name: 'Black Mamba Snake',
+        topSpeed: 16,
+        fearFactor: 9,
+        cuteness: 2,
+        lifeSpan: 11
+        
+    },
+    
+       {
+        image: "shark.jpg",
+        name: 'Great White Shark',
+        topSpeed: 25,
+        fearFactor: 10,
+        cuteness: 1,
+        lifeSpan: 
+        
+    }, 
+       
+          {
+        image: "kangaroo.jpg",
+        name: 'Red Kangaroo',
+        topSpeed: ,
+        fearFactor: 3,
+        cuteness: 8,
+        lifeSpan: 
+        
+    },
+              {
+        image: "turtle.jpg",
+        name: 'Galapagos Turtoise',
+        topSpeed: 0.27,
+        fearFactor: 1,
+        cuteness: 4,
+        lifeSpan: 100
+        
+    },
+                  {
+        image: "donkey.jpg",
+        name: 'Wild Donkey',
+        topSpeed: 64,
+        fearFactor: 4,
+        cuteness: 5,
+        lifeSpan: 
+        
+    },
+          
+
+]
+
+
---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+startGame() {
+
+    //hides start-button (class add .hide)
+
+    // function iterates through array of cards and randomly assignes a card to both players
+    assignCard() {
+    
+    }
+
+
+}
+
+
+
+
+
+// function sets the table back to default: display start-button, display card-backside
+restart() {
+
+}
+
+// eventListeners for buttons
+
+const startButton = document.querySelector('.start-button');
+startButton.addEventListener('click', startGame);
+
+
